fe90941a4f0224b8a51ba72998cc03051ddf5c3b
Redd2019 <anton-mikerin@yandex.ru>
2023-01-30 20:40:28 +0300

    Выполнено задание 2

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
+
 
     }
 
@@ -26,6 +28,27 @@ public class Main {
         }
         System.out.println("Сумма трат за месяц составила " + sum + " рублей.");
         }
+    public static void task2() {
+        System.out.println("Задача 2");
+        int[ ] arr = generateRandomArray();
+        System.out.println(Arrays.toString(arr));
+        int maxPayment = 100_000;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > maxPayment) {
+                maxPayment = arr[i];
+            }
+        }
+        System.out.println("Максимальная сумма трат за день составила " + maxPayment + " рублей.");
+        //
+        int minPayment = 200_000;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > minPayment);{
+                minPayment = arr[i];
+            }
+        }
+        System.out.println("Минимальная сумма трат за день составила " + minPayment + " рублей.");
+    }
+
 
 
     }
