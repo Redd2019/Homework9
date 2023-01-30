f5e5e3211c2fb43b3b14e2301dd04904618e060f
Redd2019 <anton-mikerin@yandex.ru>
2023-01-30 20:19:53 +0300

    Выполнено задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+
     }
-}
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+
+    public static void task1() {
+        System.out.println("Задание 1");
+        int[ ] arr = generateRandomArray();
+        System.out.println(Arrays.toString(arr));
+//
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++){
+            sum += arr[i];
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей.");
+        }
+
+
+    }
